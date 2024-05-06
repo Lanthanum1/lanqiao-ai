614ebb7ca943e92a4dfed404bb6b0a4820cd1b74
zijian <zijian.han.cs@gmail.com>
2024-05-06 17:06:23 +0000

    add 数据预处理

---
+++ 第四次模拟/数据预处理/task.py
@@ -0,0 +1,48 @@
+# conda install openpyxl lxml
+import pandas as pd
+
+
+def document_merge() -> dict:
+    data = {}
+    January, February, May = {}, {}, {}
+    excel_file = pd.read_excel("./2022_january.xlsx")
+    # for i in range(31):
+    #     January["1-"+str(excel_file.iloc[i+4,2].day)] ={excel_file.iloc[3,3]:excel_file.iloc[i+4,3],excel_file.iloc[3,4]:excel_file.iloc[i+4,4],excel_file.iloc[3,5]:excel_file.iloc[i+4,5],excel_file.iloc[3,6]:excel_file.iloc[i+4,6],excel_file.iloc[3,7]:excel_file.iloc[i+4,7],excel_file.iloc[3,8]:excel_file.iloc[i+4,8]}
+    
+    January = {
+    f"1-{excel_file.iloc[i+4, 2].day}": {
+        excel_file.iloc[3, j]: excel_file.iloc[i+4, j]
+        for j in range(3, 9)
+    }
+    for i in range(31)
+}
+    
+
+    json_file = pd.read_json("./2022_february.json")
+    for i in range(1, 29):
+        mid_data = "2-" + str(i)
+        February[mid_data] = json_file["february"][mid_data]
+
+    html_file = pd.read_html("./2022_may.html")[0]
+    first = html_file.columns.get_level_values(0).values
+    # for i in range(1, 32):
+    #     mid_data = html_file.columns.get_level_values(i).values
+    #     May[mid_data[0]] = {
+    #         first[1]: mid_data[1],
+    #         first[2]: mid_data[2],
+    #         first[3]: mid_data[3],
+    #         first[4]: mid_data[4],
+    #         first[5]: mid_data[5],
+    #         first[6]: mid_data[6],
+    #     }
+    May = {mid_data[0]: {first[j]: mid_data[j] for j in range(1, 7)} 
+       for i in range(1, 32) 
+       for mid_data in [html_file.columns.get_level_values(i).values]}
+
+    data["january"] = January
+    data["february"] = February
+    data["may"] = May
+    return data
+
+
+print(document_merge())
